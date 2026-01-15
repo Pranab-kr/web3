6d937a6e66c1e195421ad466e1d7538c832f3cf5
pranab-kr <pranabm406@gmail.com>
2026-01-15 23:55:03 +0530

    practice

---
+++ part-2/practice/src/main.rs
@@ -0,0 +1,26 @@
+// fibonacci series using recursion
+
+fn fibonacci(n: u32) -> u32 {
+    if n <= 1 {
+        return n;
+    }
+    fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+fn main() {
+    let n = 2;
+
+    if n <= 0 {
+        println!("Please enter a positive integer");
+        return;
+    };
+
+    let mut res = 0;
+    for i in 0..n {
+        res = fibonacci(i);
+      }
+
+      print!("{} ", res);
+
+      println!();
+}
